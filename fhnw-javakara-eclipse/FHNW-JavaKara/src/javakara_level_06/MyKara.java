7c7e08e1f52a2c60998bf1638768f00d06011f40
aspadola <alessandro.spadola@students.fhnw.ch>
2022-10-02 13:46:35 +0200

    Initial commit

---
+++ fhnw-javakara-eclipse/FHNW-JavaKara/src/javakara_level_06/MyKara.java
@@ -0,0 +1,34 @@
+package javakara_level_06;
+
+
+import kara.gamegrid.Kara;
+import kara.gamegrid.KaraWorld;
+
+
+/**
+ * MyKara is a subclass of Kara. Therefore, it inherits all methods of Kara: <p>
+ *  * 
+ * Actions:     move(), turnLeft(), turnRight(), putLeaf(), removeLeaf() <b>
+ * Sensors:     onLeaf(), treeFront(), treeLeft(), treeRight(), mushroomFront()
+ */
+public class MyKara extends Kara {
+	
+    /**
+     * In the 'act()' method you can write your program for Kara <br>
+     */
+	public void act() {
+		move();
+		turnRight();
+		move();
+		stop();
+	}
+	
+	/**
+	 * The main-method is the start of the program where the Kara world is loaded <br>
+	 */
+	public static void main(String[] args) {
+		KaraWorld world = new KaraWorld("WorldSetup.txt", MyKara.class);
+		//KaraWorld world = new KaraWorld("WorldSetup2.txt", MyKara.class);
+		world.show();
+	}
+}
